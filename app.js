4c88ce460929f8fba06f1aed355ddaffa49ffb3c
Frank Pizzuta <frank@pizzuta.com>
2019-03-05 18:52:25 -0500

    working

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express = require('express');
 
 var app = express();
+var users = require('./routes/users');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', './views');    // tells express where to find the views
+app.set('view engine', 'pug');  // tells express to use pug as the template engine
+
+app.use('/users', users);
+app.use('/login', users);
+
+// app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', express.static('./public'));
 app.use('/static', express.static('./public'));
 
---
+++ routes/users.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', (req, res, next)=>{
+ res.end("/users requested");
+});
+
+router.get('/login', (req, res, next)=>{
+ console.log("/users/login requested");
+ res.render('login');
+});
+
+router.get('/user', (req, res, next)=>{
+	response = {
+		username : req.query.username,
+		posts : [
+			{
+				title: 'Post 1',
+				date: '1/1/19',
+				audience: 'all',
+				message: 'This is message 1'
+			},
+			{
+				title: 'Post 2',
+				date: '1/5/19',
+				audience: 'unit1',
+				message: 'This is message 2'
+			}
+		]
+	};
+ console.log("/user requested");
+ console.log(response);
+ res.render('user',response);
+});
+
+
+module.exports = router;
